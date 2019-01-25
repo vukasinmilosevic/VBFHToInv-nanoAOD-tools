a8c18821e3902f4c78d913ed1afbd8d43b2ff7c0
amagnan <a.magnan@imperial.ac.uk>
2019-01-25 18:08:17 +0100

    adding nloSFproducer

--- crab/crab_script_vbf_mc.py
+++ crab/crab_script_vbf_mc.py
@@ -5,7 +5,7 @@ from VBFHToInv.NanoAODTools.postprocessing.VBFHToInvModules import *
 #this takes care of converting the input files from CRAB
 from PhysicsTools.NanoAODTools.postprocessing.framework.crabhelper import inputFiles,runsAndLumis
 
-p=PostProcessor('.',inputFiles(), modules=[TriggerSelectionConstructor(),JetCleaningConstructor(),puWeight2017(),btagSF2017deepCSV(),lepSFtight(),lepSFveto(),LooseMuonConstructor(),CRLooseMuonConstructor(),CRTightMuonConstructor(),LooseElectronConstructor(),CRLooseElectronConstructor(),CRTightElectronConstructor(),VLooseTauConstructor(),LoosePhotonConstructor(),MediumBJetConstructor(),MetCleaningConstructor_baseLooseLeptons(),DiCleanJetVariableConstructor(),jetmetUncertainties2017(),jecUncert_2017_MC(),CRCreatorConstructor(), SelectionCreatorConstructorLoose()],provenance=True,fwkJobReport=True)
+p=PostProcessor('.',inputFiles(), modules=[TriggerSelectionConstructor(),JetCleaningConstructor(),nloSF(),puWeight2017(),btagSF2017deepCSV(),lepSFtight(),lepSFveto(),LooseMuonConstructor(),CRLooseMuonConstructor(),CRTightMuonConstructor(),LooseElectronConstructor(),CRLooseElectronConstructor(),CRTightElectronConstructor(),VLooseTauConstructor(),LoosePhotonConstructor(),MediumBJetConstructor(),MetCleaningConstructor_baseLooseLeptons(),DiCleanJetVariableConstructor(),jetmetUncertainties2017(),jecUncert_2017_MC(),CRCreatorConstructor(),SelectionCreatorConstructorLoose()],provenance=True,fwkJobReport=True)
 p.run()
 
 print "DONE"
