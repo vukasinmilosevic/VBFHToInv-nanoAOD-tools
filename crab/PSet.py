7dd33c8edb5a69e7786c0b60cc6b57c97f91a659
amagnan <a.magnan@imperial.ac.uk>
2019-01-29 15:45:17 +0100

    additions to crab_script

--- crab/PSet.py
+++ crab/PSet.py
@@ -18,8 +18,8 @@ process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring(),
 
 process.source.fileNames = [
 	#'/eos/user/a/amagnan/datatest.root' ##you can change only this line
-       '/eos/user/a/amagnan/EWKZ2Jets_ZToLL_12Apr2018_94X_nanoAOD_test.root'
-        #'root://xrootd.grid.hep.ph.ic.ac.uk///store/mc/RunIIFall17NanoAOD/DYJetsToLL_1J_TuneCP5_13TeV-amcatnloFXFX-pythia8/NANOAODSIM/PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/310000/68FC1A44-119B-E811-82C3-1866DA890A68.root'
+       #'/eos/user/a/amagnan/EWKZ2Jets_ZToLL_12Apr2018_94X_nanoAOD_test.root'
+    'root://xrootd.grid.hep.ph.ic.ac.uk///store/mc/RunIIFall17NanoAOD/DYJetsToLL_1J_TuneCP5_13TeV-amcatnloFXFX-pythia8/NANOAODSIM/PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/310000/68FC1A44-119B-E811-82C3-1866DA890A68.root'
          #'root://cms-xrd-global.cern.ch//store/data/Run2017B/MET/NANOAOD/31Mar2018-v1/710000/D477E3DA-0A47-E811-8621-0CC47A4C8E98.root'
 ]
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(1000))
