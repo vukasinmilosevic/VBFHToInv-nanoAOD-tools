3717cda8c5cea948c5dacec2eb71c6f07505cd3f
amagnan <a.magnan@imperial.ac.uk>
2019-01-31 16:55:56 +0100

    fixing finder to find tau->e and tau->mu

--- python/postprocessing/modules/bosonDecayFinder.py
+++ python/postprocessing/modules/bosonDecayFinder.py
@@ -32,6 +32,7 @@ class bosonDecayFinder(Module):
         self.counter_tau_had = 0
 
     def endFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
+        print 'Total boson founds: %d'%(self.counter_e+self.counter_mu+self.counter_tau)
         print 'Decay to e: %d'%self.counter_e
         print 'Decay to mu: %d'%self.counter_mu
         print 'Decay to tau: %d %d'%(self.counter_tau,self.counter_tau_e+self.counter_tau_mu+self.counter_tau_had)
@@ -48,28 +49,44 @@ class bosonDecayFinder(Module):
 
         genParticles =  Collection(event, "GenPart")
         boson_found = False
+        partIdx=0
         for part in genParticles:
             if ( (abs(part.pdgId)>10 and abs(part.pdgId) < 17) and ( (part.status == 1 and (part.statusFlags & 0x1)>0) or ((part.statusFlags & 0x1)>0 and (part.statusFlags & 0x2)>0) ) ):
 
-                if (part.statusFlags & 0x2)>0: 
+                if (part.statusFlags & 0x2)>0:
                     self.counter_tau += 1
-                    if (part.pdgId==11 or abs(part.pdgId)==12):
-                        self.counter_tau_e += 1
-                        decayLeptonId = 3
-                    elif (part.pdgId==13 or abs(part.pdgId)==14):
-                        self.counter_tau_mu += 1
-                        decayLeptonId = 4
-                    elif (part.pdgId==15 or abs(part.pdgId)==16):
+                    foundLepDecay = False
+                    for taudecay in genParticles:
+                        if (taudecay.genPartIdxMother==partIdx):
+                            if (abs(taudecay.pdgId)==11 or abs(taudecay.pdgId)==12):
+                                self.counter_tau_e += 1
+                                decayLeptonId = 3
+                                foundLepDecay = True
+                                break
+                            elif (abs(taudecay.pdgId)==13 or abs(taudecay.pdgId)==14):
+                                self.counter_tau_mu += 1
+                                decayLeptonId = 4
+                                foundLepDecay = True
+                                break
+                    if (not foundLepDecay):
                         self.counter_tau_had += 1
                         decayLeptonId = 5
+                        #for taudecay in genParticles:
+                        #    if (taudecay.status == 1):
+                        #        print '-- tau prod: PdgId %d mum %d statusFlags %s'%(taudecay.pdgId,genParticles[taudecay.genPartIdxMother].pdgId,"{0:b}".format(taudecay.statusFlags))       
+
+                    break
+#                        
                 else:
-                    if (part.pdgId==11 or abs(part.pdgId)==12):
+                    if (abs(part.pdgId)==11 or abs(part.pdgId)==12):
                         self.counter_e += 1
                         decayLeptonId = 1
-                    elif (part.pdgId==13 or abs(part.pdgId)==14):
+                        break
+                    elif (abs(part.pdgId)==13 or abs(part.pdgId)==14):
                         self.counter_mu += 1
                         decayLeptonId = 2
-
+                        break
+            partIdx += 1
 
         self.out.fillBranch("decayLeptonId",decayLeptonId)
         return True
