644b56c842689b0bfa0ee47646b92aa550699115
vmilosev <vukasin.milosevic@cern.ch>
2019-02-01 11:34:34 +0100

    Update of the metCleaning module

--- python/postprocessing/modules/metCleaning.py
+++ python/postprocessing/modules/metCleaning.py
@@ -35,13 +35,13 @@ class MetCleaning(Module):
         """process event, return True (go to next module) or False (fail, go to next event)"""
         leptons1 = Collection(event, self.lep1CollectionName)
         leptons2 = Collection(event, self.lep2CollectionName)
-        
+        #print "--MAINT TEST: LEPTON COLL 1 size = ", len(leptons1), "LEPTON COLL 2 size =", len(leptons2) 
         jets_phi = getattr(event, self.jetCollectionName+"_phi")
         
         met_phi = getattr(event, self.metCollectionName+"_phi")
         met_pt = getattr(event, self.metCollectionName+"_pt")
-        
         # filling the branch for Met without leptons type 1 and mindPhi Jet/Met
+        #print "--INFO: Calling of Lep1 cleaning"
         cleanMetNoLep1_pt, cleanMetNoLep1_phi = MetCleaningProcedure(met_pt, met_phi, leptons1)
         mindPhilep1_ok, mindPhiLep1 = FormJetMetMinDphi(cleanMetNoLep1_phi, jets_phi, len(jets_phi))
         
@@ -53,7 +53,8 @@ class MetCleaning(Module):
         self.out.fillBranch("MetNo"+self.lep1CollectionName+"_phi", cleanMetNoLep1_phi)
         
         
-        # filling the branch for Met without leptons type 2
+        # filling the branch for Met without leptons type 2i
+        #print "--INFO: Calling of Lep2 cleaning"
         cleanMetNoLep2_pt, cleanMetNoLep2_phi = MetCleaningProcedure(met_pt, met_phi, leptons2)
         mindPhilep2_ok, mindPhiLep2 = FormJetMetMinDphi(cleanMetNoLep2_phi, jets_phi, len(jets_phi))
         
@@ -66,10 +67,11 @@ class MetCleaning(Module):
         self.out.fillBranch("MetNo"+self.lep2CollectionName+"_phi", cleanMetNoLep2_phi)
         
         # filling the branch for Met without leptons
-        cleanMetNoLep_pt, cleanMetNoLep_phi = MetCleaningProcedure(met_pt, met_phi, leptons1, leptons2)
+        #print "--INFO: Calling of all LEP cleaning"
+	cleanMetNoLep_pt, cleanMetNoLep_phi = MetCleaningProcedure(met_pt, met_phi, leptons1, leptons2)
         mindPhilep_ok, mindPhiLep = FormJetMetMinDphi(cleanMetNoLep_phi, jets_phi, len(jets_phi))
 
-        if mindPhilep2_ok:
+        if mindPhilep_ok:
             self.out.fillBranch("MetNoLep_"+self.jetCollectionName+"_mindPhi", mindPhiLep)
         else:
             self.out.fillBranch("MetNoLep_"+self.jetCollectionName+"_mindPhi", -1000)
@@ -82,12 +84,17 @@ class MetCleaning(Module):
 def MetCleaningProcedure(met_pt, met_phi, leptons1, leptons2 = None):
     met_x = met_pt*math.cos(met_phi)
     met_y = met_pt*math.sin(met_phi)
+   # print "Length of the first lep collection = ", len(leptons1)
     if len(leptons1)>0:
-           for lep in leptons1 :
-              met_x+=lep.pt*math.cos(lep.phi)
-              met_y+=lep.pt*math.sin(lep.phi)
+#	print "Lep coll1 ok"
+        for lep in leptons1 :
+            met_x+=lep.pt*math.cos(lep.phi)
+            met_y+=lep.pt*math.sin(lep.phi)
 
+ #   print "Test if the second lep collection is called", (leptons2!=None)
+   
     if leptons2:
+#	print "Lepton collection 2 ok, with length of:", len(leptons2)
         if len(leptons2)>0:
             for lep in leptons2 :
                 met_x+=lep.pt*math.cos(lep.phi)
@@ -101,23 +108,23 @@ def MetCleaningProcedure(met_pt, met_phi, leptons1, leptons2 = None):
     else:
         if  met_y < 0 :
             CleanMetPhi*=-1.
-
     return CleanMet, CleanMetPhi
 
 def FormJetMetMinDphi(met_phi, jet_phi, n_jets):
-    if (n_jets>0 and met_phi):
+    if (n_jets>0):
         if n_jets>=4:
             indx = 4
         else:
             indx = n_jets
-
+#	print "--Start: met_phi =", met_phi, "jet_phi[0] = ", jet_phi[0], "nJets =", n_jets
         Min = abs(deltaPhi(jet_phi[0], met_phi)) 
-       
+#      	print "--Info : mindPhi initialisation: Min =", Min  
         for i  in range(1,indx):
             phi = abs(deltaPhi(jet_phi[i], met_phi))
+ #           print "--Info: jet_phi =", jet_phi[i], " dphi =", phi
             if (Min>phi):
                 Min = phi
-        
+  #       	print "--Info : mindPhi change: Min =", Min
         return True, Min
     else:
         return False, 0.0
